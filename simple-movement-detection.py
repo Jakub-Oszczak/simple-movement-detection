f781c74ef71ed61cf59d976f5d33ee7bb442eaf8
Jakub-Oszczak <oszczak.jakub@gmail.com>
2022-03-23 14:58:58 +0100

    first commit

---
+++ simple-movement-detection.py
@@ -0,0 +1,31 @@
+import cv2
+
+
+webcam = cv2.VideoCapture(0)
+first_run = True
+
+while True:
+    satus, frame = webcam.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+    if not first_run:
+        subtracted = cv2.subtract(blurred, previous_frame)
+        (T, threshInv) = cv2.threshold(subtracted, 40, 255, cv2.THRESH_BINARY)
+        dilated = cv2.dilate(threshInv, None, iterations=2)
+        contours, _ = cv2.findContours(threshInv, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        for contour in contours:
+            cv2.drawContours(frame, contour, -1, (0, 255, 0), 4)
+
+        cv2.imshow("Mask", dilated)
+
+    previous_frame = blurred
+
+    key = cv2.waitKey(10)
+
+    if key==81 or key==113 or key==27:
+        break
+    
+    first_run = False
+
+webcam.release()
